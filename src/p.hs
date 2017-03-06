cbd6255ec05c4cb2ffb85c50aa009b4f52547211
7akase <git_config@user.email>
2017-03-06 13:14:49 +0000

    add some parsers

--- src/p.hs
+++ src/p.hs
@@ -1,7 +1,10 @@
-import Control.Arrow
-import Data.Functor
-import Control.Applicative
-import Control.Monad
+import Control.Arrow ((***))
+import Data.Functor (fmap)
+import Control.Applicative (pure, (<*>))
+import Control.Monad (return , (>>=))
+
+import Data.Char (isLower, isDigit)
+import Prelude hiding (fail)
 
 newtype Parser a = Parser (String -> [(a, String)])
 
@@ -25,5 +28,31 @@ instance Monad Parser where
   return x = Parser (\s -> [(x, s)])
   p >>= q = Parser (\s -> [(y, s'') | (x, s') <- apply p s
                                     , (y, s'') <- apply (q x) s'])
+
+getc :: Parser Char
+getc = Parser f
+         where
+           f [] = []
+           f (c:cs) = [(c,cs)]
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do { c <- getc; if p c then return c else fail }
+
+fail = Parser (\s -> [])
+
+char :: Char -> Parser ()
+char x = do { c <- sat (== x); return () }
+
+string :: String -> Parser ()
+string [] = return ()
+string (x : xs)  = do {char x; string xs; return () }
+
+lower :: Parser Char
+lower = sat isLower
+
+digit :: Parser Int
+digit = do {d <- sat isDigit; return (cvt d)}
+          where
+            cvt d = fromEnum d - fromEnum '0'
 
 
